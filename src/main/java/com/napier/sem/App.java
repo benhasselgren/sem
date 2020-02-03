89918aa9f278769dcb0e45faaba8b327a2d57951
Ben Hasselgren <ben.nadalito@gmail.com>
2020-02-03 14:48:39 +0000

    Added sql query to be able to view an employees record

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -11,6 +11,10 @@ public class App
 
         // Connect to database
         a.connect();
+        // Get Employee
+        Employee emp = a.getEmployee(255530);
+        // Display results
+        a.displayEmployee(emp);
 
         // Disconnect from database
         a.disconnect();
@@ -82,15 +86,22 @@ public class App
     }
     public Employee getEmployee(int ID)
     {
+
         try
         {
             // Create an SQL statement
             Statement stmt = con.createStatement();
             // Create string for SQL statement
             String strSelect =
-                    "SELECT emp_no, first_name, last_name "
-                            + "FROM employees "
-                            + "WHERE emp_no = " + ID;
+                    "SELECT employees.emp_no, employees.first_name, employees.last_name, titles.title, salaries.salary, departments.dept_name, (SELECT CONCAT(emp_no, \" \", first_name, \" \", last_name) FROM employees WHERE emp_no = dept_manager.emp_no) AS manager FROM employees "
+                            + "JOIN salaries on  employees.emp_no=salaries.emp_no "
+                            + "JOIN titles on  employees.emp_no=titles.emp_no "
+                            + "JOIN dept_emp on  employees.emp_no=dept_emp.emp_no "
+                            + "JOIN departments on  dept_emp.dept_no=departments.dept_no "
+                            + "JOIN dept_manager on  departments.dept_no=dept_manager.dept_no "
+                            + "WHERE employees.emp_no = " + ID + " "
+                            + "AND dept_emp.to_date = '9999-01-01' AND dept_manager.to_date = '9999-01-01' "
+                            + "LIMIT 1; ";
             // Execute SQL statement
             ResultSet rset = stmt.executeQuery(strSelect);
             // Return new employee if valid.
@@ -101,6 +112,10 @@ public class App
                 emp.emp_no = rset.getInt("emp_no");
                 emp.first_name = rset.getString("first_name");
                 emp.last_name = rset.getString("last_name");
+                emp.salary =  rset.getInt("salary");
+                emp.title = rset.getString("title");
+                emp.dept_name = rset.getString("dept_name");
+                emp.manager= rset.getString("manager");
                 return emp;
             }
             else
@@ -111,6 +126,21 @@ public class App
             System.out.println(e.getMessage());
             System.out.println("Failed to get employee details");
             return null;
+        }
+    }
+
+    public void displayEmployee(Employee emp)
+    {
+        if (emp != null)
+        {
+            System.out.println(
+                    emp.emp_no + " "
+                            + emp.first_name + " "
+                            + emp.last_name + "\n"
+                            + emp.title + "\n"
+                            + "Salary:" + emp.salary + "\n"
+                            + emp.dept_name + "\n"
+                            + "Manager: " + emp.manager + "\n");
         }
     }
 }
